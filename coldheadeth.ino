51bc2fef50b0ede7da22a7995d71663207f3e00b
jgort71645 <49799359+jgort71645@users.noreply.github.com>
2019-07-05 12:19:17 -0700

    Update coldheadeth.ino

--- coldheadeth.ino
+++ coldheadeth.ino
@@ -2,10 +2,10 @@
 #include <ETH.h>
 
 //Define ADC interfaces
-#define CS_PIN D4     // ESP8266 default SPI pins
-#define CLOCK_PIN D5  // Should work with any other GPIO pins, since the library does not formally
-#define MOSI_PIN D7   // use SPI, but rather performs pin bit banging to emulate SPI communication.
-#define MISO_PIN D6   //
+#define CS_PIN **     // ESP8266 default SPI pins
+#define CLOCK_PIN 31  // Should work with any other GPIO pins, since the library does not formally
+#define MOSI_PIN **   // use SPI, but rather performs pin bit banging to emulate SPI communication.
+#define MISO_PIN **   //
 #define NCP3204 4     // MCP No.
 MCP320X mcp3204 = MCP320X(MCP3204, CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
 
